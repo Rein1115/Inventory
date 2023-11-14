b19440dee9d878b253cce497cfcdadfa2461cf51
Rein1115 <142583344+Rein1115@users.noreply.github.com>
2023-11-14 09:26:54 +0800

    database column name status

--- database/migrations/2023_11_11_234048_create_products_table.php
+++ database/migrations/2023_11_11_234048_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('quantity');
             $table->decimal('price', 8, 2); 
             $table->date('expiration'); 
+            $table->integer('status')->default(0);
             $table->timestamps(); 
         });
     }
