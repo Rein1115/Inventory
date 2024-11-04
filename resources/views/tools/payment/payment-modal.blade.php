2e74b27f31a57273d86a6c53cdc805c429e7dd05
rein1115 <reinjunelaride@gmail.com>
2024-11-04 08:13:28 +0800

    UPDATE

--- resources/views/tools/payment/payment-modal.blade.php
+++ resources/views/tools/payment/payment-modal.blade.php
@@ -31,7 +31,33 @@
                           <option id="Gcash" value="Gcash">Gcash</option>
                           <option id="Paymaya" value="Paymaya">Paymaya</option>    
                           <option id="Paypal" value="Paypal">Paypal</option>
-                          <option id="Banktransfer" value="banktransfer">Bank Transfers</option>
+                          <option id="Banktransfer" value="Bank Transfers">Bank Transfers</option>
+
+                           <!-- Philippine Universal and Commercial Banks -->
+                          <option id="BDO" value="BDO Unibank">BDO Unibank</option>
+                          <option id="Metrobank" value="Metropolitan Bank & Trust Company (Metrobank)">Metropolitan Bank & Trust Company (Metrobank)</option>
+                          <option id="BPI" value="Bank of the Philippine Islands (BPI)">Bank of the Philippine Islands (BPI)</option>
+                          <option id="LandBank" value="Land Bank of the Philippines">Land Bank of the Philippines</option>
+                          <option id="PNB" value="Philippine National Bank">Philippine National Bank</option>
+                          <option id="ChinaBank" value="China Banking Corporation">China Banking Corporation</option>
+                          <option id="SecurityBank" value="Security Bank Corporation">Security Bank Corporation</option>
+                          <option id="UnionBank" value="Union Bank of the Philippines">Union Bank of the Philippines</option>
+                          <option id="RCBC" value="Rizal Commercial Banking Corporation (RCBC)">Rizal Commercial Banking Corporation (RCBC)</option>
+                          <option id="AUB" value="Asia United Bank">Asia United Bank</option>
+                          <option id="PBCOM" value="Philippine Bank of Communications">Philippine Bank of Communications</option>
+                          <option id="EastWestBank" value="East West Banking Corporation">East West Banking Corporation</option>
+                          <option id="RobinsonsBank" value="Robinsons Bank Corporation">Robinsons Bank Corporation</option>
+
+                          <!-- Government-Owned Banks -->
+                          <option id="LandBank" value="Land Bank of the Philippines">Land Bank of the Philippines</option>
+                          <option id="DBP" value="Development Bank of the Philippines">Development Bank of the Philippines (DBP)</option>
+
+                          <!-- Foreign Banks in the Philippines -->
+                          <option id="Citibank" value="Citibank">Citibank, N.A.</option>
+                          <option id="HSBC" value="HSBC">Hongkong and Shanghai Banking Corporation (HSBC)</option>
+                          <option id="StandardChartered" value="Standard Chartered">Standard Chartered Bank</option>
+                          <option id="JPMorgan" value="JPMorgan Chase">JP Morgan Chase Bank</option>
+                          <option id="BankOfAmerica" value="Bank of America">Bank of America, N.A.</option>
                       </select>
                     </div>
                 </div>
